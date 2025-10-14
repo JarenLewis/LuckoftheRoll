9858cfd688cec468f6b8029e6a2ade1f4d2bf62a
--replace-all <jarendlewis@gmail.com>
2025-10-14 16:57:30 -0400

    Simplified Main

--- src/main/java/org/example/Coin.java
+++ src/main/java/org/example/Coin.java
@@ -1,17 +1,87 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class Coin {
 
-    public static int coinFace() {
+    static Scanner input = new Scanner(System.in);
+
+    public static int coinFace() throws InterruptedException {
 
         Random coinFace = new Random();
+
         int face = coinFace.nextInt(2);
 
-        System.out.println(face);
+        System.out.println("Time to flip");
+        Thread.sleep(800);
+        System.out.println("Heads");
+        Thread.sleep(400);
+        System.out.println("Tails");
+        Thread.sleep(400);
+        System.out.println("Heads");
+        Thread.sleep(400);
+        System.out.println("Tails");
+        Thread.sleep(400);
+        System.out.println("Heads");
+        Thread.sleep(400);
+        System.out.println("Tails");
+        Thread.sleep(400);
+        System.out.println("Heads");
+        Thread.sleep(400);
+        System.out.println("Tails");
+        Thread.sleep(400);
+        System.out.println("Heads");
+        Thread.sleep(400);
+        System.out.println("Tails");
+        Thread.sleep(400);
+        System.out.println("Heads");
+
+
+
+
+
+
+//        Thread.sleep(4000);
+//        System.out.println("2");
+//        Thread.sleep(4000);
+//        System.out.println("1");
+//        Thread.sleep(4000);
+
+        if (face == 0) {
+            System.out.println("Heads");
+        }
+        else {
+            System.out.println("Tails");
+        }
 
         return face;
     }
 
+    public static int coinChosen (int inputAnswer) throws InterruptedException {
+        System.out.println("We'll use a coin");
+
+        Thread.sleep(800);
+
+        System.out.println("");
+        System.out.println("Do you want to choose the side of the coin? Y / N");
+        String answer = input.nextLine().toUpperCase();
+
+        if (answer.contains("Y")) {
+            System.out.println("Make your choice.");
+
+            Thread.sleep(800);
+
+            System.out.println("");
+            System.out.println("[H]eads");
+            System.out.println("[T]ails");
+
+
+        }
+        else {
+            Coin.coinFace();
+        }
+        return inputAnswer;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,61 +8,18 @@ import java.util.Scanner;
 //change the choices to the first letter. The numbers are confusing you.
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("How many sides do you need?");
+        System.out.println("How many sides are required?");
         String answer = input.nextLine();
         int inputAnswer = Integer.parseInt(answer);
 
-        System.out.println("Do you want to choose the side of the coin? Yes[1] No[2]");
-        answer = input.nextLine();
-        int coinChoice = Integer.parseInt(answer);
-
-
-        int faceAnswer = 0;
-        if (coinChoice == 1) {
-            System.out.println("Heads[1] or Tails[2]?");
-            answer = input.nextLine();
-            faceAnswer = Integer.parseInt(answer);
-//            if (faceAnswer == Coin.coinFace())
+        if (inputAnswer == 2) {
+            Coin.coinChosen(inputAnswer);
         }
-
-        if (faceAnswer == 2) {
-            Coin.coinFace();
-            System.out.println("It was");
-        }
-        
-        ;
-
-
-//            if (faceAnswer == 1 || faceAnswer == 2){
-
-
-//
-//        if (inputAnswer == 2) {
-//            System.out.println("Flip");
-//            answer = input.nextLine();
-//            int flip = Integer.parseInt(answer);
-//
-//           Coin.coinFace(flip);
-
-
-//            int face = coinFace.nextInt(2);
-//
-//            System.out.println("Choose Heads or Tails: ");
-//            String coinChoice = input.nextLine();
-//            System.out.println("Choice: " + coinChoice);
-//
-//            int chosenFace = Integer.parseInt(coinChoice);
-//
-//
-//            }
-
+//        else if (inputAnswer == 6)
     }
+}
 
-
-
-
-    }
